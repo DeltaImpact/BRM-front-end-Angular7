8e3e1fca0df6da864bfa8fb5c76511605a04518b
DeltaImpact <p.grachev.vik@gmail.com>
2019-02-18 15:43:24 +0300

    feat Updated root-store selectors.

--- src/app/root-store/selectors.ts
+++ src/app/root-store/selectors.ts
@@ -1,26 +1,20 @@
 import { createSelector, MemoizedSelector } from "@ngrx/store";
 import { RolesSelectors } from "./roles-store";
 import { PermissionsSelectors } from "./permission-store";
-
-export const selectError: MemoizedSelector<object, string> = createSelector(
-  //     RolesSelectors.selectRolesState,
-  //     RolesPermission.selectMyOtherFeatureError,
-  //   (myFeatureError: string, myOtherFeatureError: string) => {
-  //     return myFeature || myOtherFeature;
-  //   }
-  RolesSelectors.selectRolesState,
-  (error: any) => error
-);
+import { UsersSelectors } from "./user-store";
 
 export const selectIsLoading: MemoizedSelector<
   object,
   boolean
 > = createSelector(
-  //   MyFeatureStoreSelectors.selectMyFeatureIsLoading,
-  //   MyOtherFeatureStoreSelectors.selectMyOtherFeatureIsLoading,
-  //   (myFeature: boolean, myOtherFeature: boolean) => {
-  //     return myFeature || myOtherFeature;
-  //   }
   RolesSelectors.selectRolesIsLoading,
-  (isLoading: boolean) => isLoading
+  PermissionsSelectors.selectPermissionsIsLoading,
+  UsersSelectors.selectUsersIsLoading,
+  (
+    rolesLoading: boolean,
+    permissionsLoading: boolean,
+    usersLoading: boolean
+  ) => {
+    return rolesLoading || permissionsLoading || usersLoading;
+  }
 );
